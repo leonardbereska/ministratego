88d0d560628a240b79b528c9dea13f4893bfc9a7
maichmueller <m.aichmueller@gmail.com>
2017-12-15 20:39:23 +0100

    fully implemented is_legal, updated agent board, added updateBoard and others

--- agent.py
+++ agent.py
@@ -10,6 +10,10 @@ import pieces
 class Agent:
     def __init__(self, team):
         self.team = team
+        self.board = np.empty((5, 5), dtype=object)
+        opp_setup = np.array([pieces.Piece(88, (self.team +1) % 2 )]*10, dtype=object)
+        opp_setup.resize(2,5)
+        self.board[3:5, 0:5] = opp_setup
 
     def decide_setup(self, types_available):
         """
@@ -24,10 +28,14 @@ class Agent:
         for i, piece in enumerate(pieces_setup):
             setup[i] = piece
         setup.resize(2, 5)
+        self.board[0:2, 0:5] = setup
         return setup
+
+    def updateBoard(self, updatedPieces):
+        for pos, piece in updatedPieces:
+            self.board[pos] = piece
 
     def decide_move(self, state, actions):
         # ignore state, do random action
         action = random.choice(actions)
         return action
-
