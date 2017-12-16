5a2c5b5962bee5b615339f9416b382ff3b5aac37
Leonard Bereska <leonard.bereska@hotmail.de>
2017-12-16 09:21:57 +0100

    run random agent simulation (each 100 games) of 1000 initial setups

--- agent.py
+++ agent.py
@@ -5,22 +5,34 @@ Agent decides the initial setup and decides which action to take
 import random
 import numpy as np
 import pieces
+import copy
 
 
 class Agent:
     def __init__(self, team):
         self.team = team
         self.board = np.empty((5, 5), dtype=object)
-        opp_setup = np.array([pieces.Piece(88, (self.team +1) % 2 )]*10, dtype=object)
-        opp_setup.resize(2,5)
+        opp_setup = np.array([pieces.Piece(88, (self.team + 1) % 2)]*10, dtype=object)
+        opp_setup.resize(2, 5)
         self.board[3:5, 0:5] = opp_setup
+
+    def init_setup(self, types_available):
+        """
+        Agent has to implement how to order his initial figures
+        """
+        raise NotImplementedError
 
     def decide_setup(self, types_available):
         """
-        Choose an initial setup of the available figure types
+        Communicate initial setup of the available figure types to game
         """
-        # randomly order the available figures in a list
-        types_setup = np.random.choice(types_available, 10, replace=False)
+        types_setup = self.init_setup(types_available)
+
+        # check if used only types available
+        check_setup = copy.deepcopy(types_setup)
+        check_setup.sort()
+        setup_valid = check_setup == types_available
+        assert(setup_valid.all()), "cheated in setup!"
 
         # converting list of types to an 2x5 array of Pieces
         pieces_setup = [pieces.Piece(i, self.team) for i in types_setup]
@@ -34,6 +46,43 @@ class Agent:
     def updateBoard(self, updatedPieces):
         for pos, piece in updatedPieces:
             self.board[pos] = piece
+
+    def decide_move(self, state, actions):
+        """
+        Agent has to implement on which action to decide given the state
+        """
+        raise NotImplementedError
+
+
+class RandomAgent(Agent):
+    """
+    Agent who chooses his initial setup and actions at random
+    """
+    def __init__(self, team):
+        super(RandomAgent, self).__init__(team=team)
+
+    def init_setup(self, types_available):
+        # randomly order the available figures in a list
+        return np.random.choice(types_available, 10, replace=False)
+
+    def decide_move(self, state, actions):
+        # ignore state, do random action
+        action = random.choice(actions)
+        return action
+
+
+class SmartSetup(Agent):
+    """
+    RandomAgent with smart initial setup
+    """
+    def __init__(self, team, setup):
+        super(SmartSetup, self).__init__(team=team)
+        self.setup = setup
+
+    def init_setup(self, types_available):
+        # cheating Agent: Agent 0 gets reward 100 in 100 simulations
+        # [11, 10, 3, 10, 11, 3, 3, 3, 3, 10]
+        return self.setup
 
     def decide_move(self, state, actions):
         # ignore state, do random action
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import numpy as np
-import scipy
-import random
+from scipy import spatial
 from matplotlib import pyplot as plt
-
 import pieces
 import agent
 import copy
+import pickle
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, setup):
         """
         player1 = Player()
         player2 = Player()
@@ -18,20 +17,18 @@ class Game:
         self.board[0:1, 0:5] = player1.decideInitialSetup()
         self.board[3:4, 0:5] = player2.decideInitialSetup()
         """
-        self.agent0 = agent.Agent(0)
-        self.agent1 = agent.Agent(1)
-
+        self.agents = (agent.SmartSetup(0, setup), agent.RandomAgent(1))
         self.board = np.empty((5, 5), dtype=object)
 
-        self.types_available = [1, 2, 2, 2, 3, 3, 10, 11, 11, 0]
-        setup0 = self.agent0.decide_setup(self.types_available)
-        setup1 = self.agent1.decide_setup(self.types_available)
+        self.types_available = [0, 1, 2, 2, 2, 3, 3, 10, 11, 11]
+        setup0 = self.agents[0].decide_setup(self.types_available)
+        setup1 = self.agents[1].decide_setup(self.types_available)
         setup1 = np.flip(setup1, 0)  # flip setup for second player
         self.board[3:5, 0:5] = setup0
         self.board[0:2, 0:5] = setup1
-        self.board[2, 2] = pieces.Piece(99, 99)  # initialize obstacle
+        self.board[2, 2] = pieces.Piece(99, 99)  # set obstacle
 
-        self.turn = 1
+        self.move_count = 1  # agent 1 starts
 
         self.deadFigures = []
         self.deadFigures.append([])
@@ -39,63 +36,85 @@ class Game:
 
         self.battleMatrix = dict()
         self.battleMatrix[1, 11] = -1
-        self.battleMatrix[1, 1] = 2
+        self.battleMatrix[1, 1] = 0
         self.battleMatrix[1, 2] = -1
         self.battleMatrix[1, 3] = -1
+        self.battleMatrix[1, 0] = 1
         self.battleMatrix[1, 10] = 1
         self.battleMatrix[2, 0] = 1
         self.battleMatrix[2, 11] = -1
         self.battleMatrix[2, 1] = 1
-        self.battleMatrix[2, 2] = 2
+        self.battleMatrix[2, 2] = 0
         self.battleMatrix[2, 3] = -1
         self.battleMatrix[2, 10] = -1
         self.battleMatrix[3, 0] = 1
         self.battleMatrix[3, 11] = 1
         self.battleMatrix[3, 2] = 1
-        self.battleMatrix[3, 3] = 2
-        self.battleMatrix[3, 1] = 11
+        self.battleMatrix[3, 3] = 0
+        self.battleMatrix[3, 1] = 1
         self.battleMatrix[3, 10] = -1
         self.battleMatrix[10, 0] = 1
         self.battleMatrix[10, 11] = -1
         self.battleMatrix[10, 1] = 1
         self.battleMatrix[10, 2] = 1
         self.battleMatrix[10, 3] = 1
-        self.battleMatrix[10, 10] = 2
+        self.battleMatrix[10, 10] = 0
 
     def run_game(self):
-        Tie = False
-        while True:
-            visible_state, actions_possible = self.get_agent_knowledge()
-            if not actions_possible:
-                return (self.turn + 1) % 2  # other player won
-            if self.turn % 2 == 1:
-                new_move = self.agent0.decide_move(visible_state, actions_possible)
-            else:
-                new_move = self.agent1.decide_move(visible_state, actions_possible)
-            if not self.is_legal_move(new_move):
-                print('Warning, player chose illegal move!')
-            else:
-                self.do_move(new_move)
-            print(self.board)
-            decision = self.goal_test()
-            if decision:
-                break
-            elif decision == 0:
-                Tie = True
-            if self.goal_test(actions_possible):
-                break
-            self.turn += 1
-        if Tie:
-            return 0.5
-        elif self.turn % 2 == 1:
-            return 1  # player1 won!
-        else:
-            return 0  # player2 won
+        game_over = False
+        rewards = None
+        while not game_over:
+            rewards = self.run_step()
+            if rewards is not None:
+                game_over = True
+        return rewards
+
+    def run_step(self):
+        visible_state, actions_possible = self.get_agent_knowledge()
+        turn = self.move_count % 2  # player 1 or player 0
+
+        # test if game is over
+        if self.goal_test(actions_possible):  # flag already discovered or no action possible
+            if turn == 1:
+                return 1, -1  # agent0 wins
+            elif turn == 0:
+                return -1, 1  # agent1 wins
+        if self.move_count > 1000:  # if game lasts longer than 1000 turns => tie
+            return 0, 0  # each agent gets reward 0
+
+        new_move = self.agents[turn].decide_move(visible_state, actions_possible)
+        self.do_move_proxy(new_move)  # execute agent's choice
+        self.move_count += 1
+        return None
+
+    def do_move_proxy(self, move):
+        """
+        replaces do_move function with working proxy until update_moves is implemented
+        assumes move has been checked to be legal previously
+        """
+        from_pos = move[0]
+        to_pos = move[1]
+        from_piece = self.board[from_pos]
+        to_piece = self.board[to_pos]
+
+        if to_piece is None:  # if field empty then simply move there
+            self.board[to_pos] = from_piece
+            self.board[from_pos] = None
+        else:  # if not empty field => fight
+            outcome = self.fight(from_piece, to_piece)
+            if outcome == 1:  # attacker wins
+                self.board[from_pos] = None
+                self.board[to_pos] = from_piece
+            elif outcome == -1:  # defendant wins
+                self.board[from_pos] = None
+                self.board[to_pos] = to_piece  # piece stays
+            elif outcome == 0:  # tie: both die
+                self.board[from_pos] = None
+                self.board[to_pos] = None
 
     def do_move(self, move):
         """
         :param move: tuple or array consisting of coordinates from in 0 and to in 1
-        :return:
         """
         from_ = move[0]
         to_ = move[1]
@@ -107,69 +126,68 @@ class Game:
                 print('Warning, cant let pieces of same team fight!')
                 return False
             elif fight_outcome == 1:
-                self.updateBoard( (to_, self.board[from_]), True)
-                self.updateBoard( (from_, None), True)
-            elif fight_outcome == 2:
-                self.updateBoard( (to_, None), True)
-                self.updateBoard( (from_, None), True)
+                self.update_board((to_, self.board[from_]), True)
+                self.update_board((from_, None), True)
+            elif fight_outcome == 0:
+                self.update_board((to_, None), True)
+                self.update_board((from_, None), True)
             else:
-                self.updateBoard( (from_, None), True)
+                self.update_board((from_, None), True)
         else:
-            self.updateBoard( [(from_, None), (to_, self.board[from_])], False)
+            self.update_board([(from_, None), (to_, self.board[from_])], False)
         return True
 
-    def updateBoard(self, updatedPieces, visible):
+    def update_board(self, updated_pieces, visible):
         """
-        :param updatedPieces: array of tuples (piece_board_position, piece_object)
+        :param updated_pieces: array of tuples (piece_board_position, piece_object)
         :param visible: boolean, True if the piece is visible to the enemy team, False if hidden
         :return: void
         """
         if visible:
-            self.agent0.updateBoard(updatedPieces)
-            self.agent1.updateBoard(updatedPieces)
+            self.agent0.updateBoard(updated_pieces)
+            self.agent1.updateBoard(updated_pieces)
         else:
-            if not updatedPieces[1] is None:
-                if updatedPieces[1].team == 0:
-                    self.agent0.updateBoard(updatedPieces)
-                    self.agent1.updateBoard( (updatedPieces[0], pieces.Piece(88, 1)) )
+            if not updated_pieces[1] is None:
+                if updated_pieces[1].team == 0:
+                    self.agent0.updateBoard(updated_pieces)
+                    self.agent1.updateBoard((updated_pieces[0], pieces.Piece(88, 1)))
                 else:
-                    self.agent0.updateBoard( (updatedPieces[0], pieces.Piece(88, 0)) )
-                    self.agent1.updateBoard(updatedPieces)
+                    self.agent0.updateBoard((updated_pieces[0], pieces.Piece(88, 0)))
+                    self.agent1.updateBoard(updated_pieces)
             else:
-                self.agent0.updateBoard(updatedPieces)
-                self.agent1.updateBoard(updatedPieces)
+                self.agent0.updateBoard(updated_pieces)
+                self.agent1.updateBoard(updated_pieces)
 
-        for pos, piece in updatedPieces:
+        for pos, piece in updated_pieces:
             self.board[pos] = piece
 
     def fight(self, piece_att, piece_def):
         """
-        Determine the outcome of a fight between two pieces
+        Determine the outcome of a fight between two pieces: 1: win, 0: tie, -1: loss
+        add dead pieces to deadFigures
         """
-        if piece_att.team == piece_def.team:
-            return None  # same team cant fight
         outcome = self.battleMatrix[piece_att.type, piece_def.type]
         if outcome == 1:
             self.deadFigures[piece_def.team].append(piece_def.type)
-        elif outcome == 2:
+        elif outcome == 0:
             self.deadFigures[piece_def.team].append(piece_def.type)
             self.deadFigures[piece_att.team].append(piece_att.type)
-        else:
+        elif outcome == -1:
             self.deadFigures[piece_att.team].append(piece_att.type)
         return outcome
 
-    def is_legal_move(self, moveToCheck):
+    def is_legal_move(self, move_to_check):
         """
 
-        :param moveToCheck: array/tuple with the coordinates of the position from and to
+        :param move_to_check: array/tuple with the coordinates of the position from and to
         :return: True if warrants a legal move, False if not
         """
-        pos_before = moveToCheck[0]
-        pos_after = moveToCheck[1]
+        pos_before = move_to_check[0]
+        pos_after = move_to_check[1]
 
         if self.board[pos_before] is None:
             return False  # no piece on field to move
-        move_dist = scipy.spatial.distance.cityblock(pos_before, pos_after)
+        move_dist = spatial.distance.cityblock(pos_before, pos_after)
         if move_dist > self.board[pos_before].move_radius:
             return False  # move too far for selected piece
         if move_dist > 1:
@@ -189,15 +207,17 @@ class Game:
         if not self.board[pos_after] is None:
             if self.board[pos_after].team == self.board[pos_before].team:
                 return False  # cant fight own pieces
+            if self.board[pos_after].type == 99:
+                return False  # cant fight obstacles
         return True
 
     def goal_test(self, actions_possible):
         if 0 in self.deadFigures[0] or 0 in self.deadFigures[1]:
+            # print('flag captured')
             return True
         elif not actions_possible:
+            # print('cannot move anymore')
             return True
-        #TODO: implement tie checkup to avoid near endless loops
-
         else:
             return False
 
@@ -206,8 +226,8 @@ class Game:
         :return: a deepcopy of the board with pieces visible to the agent, and his possible actions
         """
         board_visible = copy.deepcopy(self.board)
-        same_team = self.turn % 2
-        other_team = (self.turn + 1) % 2
+        same_team = self.move_count % 2
+        other_team = (self.move_count + 1) % 2
         actions_possible = []
         # TODO: CLean up visible states update and only leave possible actions calc behind
         for pos in ((i, j) for i in range(5) for j in range(5)):
@@ -233,8 +253,8 @@ def print_board(board):
     Plots a board object in a pyplot figure
     """
     board = copy.deepcopy(board)  # ensure to not accidentally change input
-    plt.interactive(False)  # make plot stay?
-    plt.figure()
+    plt.interactive(False)  # make plot stay? true: close plot, false: keep plot
+    fig = plt.figure()
     layout = np.add.outer(range(5), range(5)) % 2  # chess-pattern board
     plt.imshow(layout, cmap=plt.cm.magma, alpha=.5, interpolation='nearest')  # plot board
     for pos in ((i, j) for i in range(5) for j in range(5)):  # go through all board positions
@@ -259,8 +279,40 @@ def print_board(board):
             plt.plot(pos[1], pos[0], piece_marker, markersize=37)  # plot markers for pieces
             plt.annotate(str(piece), xy=(pos[1], pos[0]), size=20, ha="center", va="center")  # piece type on marker
     plt.show()
-    return 1
+    return fig
 
 
-new = Game()
-print_board(new.board)
+def simulation():
+    """
+
+    :return: tested_setups: list of setup and winning percentage
+    """
+    types_available = [0, 1, 2, 2, 2, 3, 3, 10, 11, 11]
+    num_simulations = 100
+    num_setups = 1000
+    tested_setups = []
+
+    for i in range(num_setups):  # test 100 setups
+        setup = np.random.choice(types_available, 10, replace=False)
+        win_count = 0
+
+        for simu in range(num_simulations):  # simulate games
+            new = Game(setup)
+            # if simu % 10 == 0:
+            #     print('\nTotal rewards: {}, Simulation {}/{}'.format(total_reward, simu, num_simu))
+            for step in range(2000):
+                game_reward = new.run_step()
+                if game_reward is not None:
+                    if game_reward[0] == 1:  # count wins
+                        win_count += 1
+                    break
+        tested_setups.append((setup, win_count/num_simulations))
+        print('\nAgent wins {} out of {} simulations'
+              '\nSetup {} of {}'.format(win_count, num_simulations, i+1, num_setups))
+    return tested_setups
+
+
+# setups = simulation()
+# pickle.dump(setups, open('randominit2.p', 'wb'))
+
+
